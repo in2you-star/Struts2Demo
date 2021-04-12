f416a05dada9843b2eb678b2af75ab27e3f357a1
in2you-star <2425773708@qq.com>
2021-04-12 15:12:45 +0800

    在Student实体类中新增一个无参和有参的构造方法

--- src/main/java/com/myIt/pojo/Student.java
+++ src/main/java/com/myIt/pojo/Student.java
@@ -4,6 +4,14 @@ public class Student {
     private int id;
     private String name;
 
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
     public int getId() {
         return id;
     }
